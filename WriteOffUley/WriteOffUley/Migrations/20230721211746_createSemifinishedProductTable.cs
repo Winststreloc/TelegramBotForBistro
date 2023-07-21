b53af231edf53a7f99c79c1e62fe4037bc9e8fa4
Winststreloc <goloologo@gmail.com>
2023-07-22 01:41:31 +0300

    merge

---
+++ WriteOffUley/WriteOffUley/Migrations/20230721211746_createSemifinishedProductTable.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WriteOffUley.Migrations
+{
+    public partial class createSemifinishedProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSemiFinshedProducts_Products_ProudctId",
+                table: "ProductSemiFinshedProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "ProudctId",
+                table: "ProductSemiFinshedProducts",
+                newName: "ProductId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LiquidOrSolid",
+                table: "SemiFinishedProducts",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Quantity",
+                table: "ProductSemiFinshedProducts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSemiFinshedProducts_Products_ProductId",
+                table: "ProductSemiFinshedProducts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSemiFinshedProducts_Products_ProductId",
+                table: "ProductSemiFinshedProducts");
+
+            migrationBuilder.DropColumn(
+                name: "LiquidOrSolid",
+                table: "SemiFinishedProducts");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "ProductSemiFinshedProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "ProductSemiFinshedProducts",
+                newName: "ProudctId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSemiFinshedProducts_Products_ProudctId",
+                table: "ProductSemiFinshedProducts",
+                column: "ProudctId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ WriteOffUley/WriteOffUley/Migrations/20230721221804_createSemifinishedProductTable4.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WriteOffUley.Migrations
+{
+    public partial class createSemifinishedProductTable4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Operations_Users_UserId",
+                table: "Operations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Operations_UserId",
+                table: "Operations");
+
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "Operations");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Operations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "Operations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Operations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operations_UserId",
+                table: "Operations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Operations_Users_UserId",
+                table: "Operations",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
